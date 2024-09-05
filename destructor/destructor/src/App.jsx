ced4d7e5cd28a64b54edc290e6c345cb5d26b16c
Meet-Shingala2467 <meetshingala5557@gmail.com>
2024-09-05 10:20:09 +0530

    2

---
+++ destructor/destructor/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+     
+    <h1>Welcome to the App!</h1>
+    <p>This is the starting point of our application.</p>
+    </div>
+  )
+}
+
+export default App
