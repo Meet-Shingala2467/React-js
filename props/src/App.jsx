89867701d8d4cbdf1e165e9be15e1dae32c30a63
Meet-Shingala2467 <meetshingala5557@gmail.com>
2024-09-12 09:20:21 +0530

    15

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -4,41 +4,77 @@ function App() {
     {
       id: 1,
       name: "John Doe",
-      email: "john.doe@example.com"
+      email: "john.doe@example.com",
     },
     {
       id: 2,
       name: "Jane Doe",
-      email: "jane.doe@example.com"
+      email: "jane.doe@example.com",
     },
     {
       id: 3,
       name: "Alice Smith",
-      email: "alice.smith@example.com"
-    }
+      email: "alice.smith@example.com",
+    },
   ];
   const colors = [
     {
       id: 1,
-      color: "blue"
+      color: "blue",
     },
     {
       id: 2,
-      color: "red"
+      color: "red",
     },
     {
       id: 3,
-      color: "green"
-    }
+      color: "green",
+    },
   ];
-    console.log(users)
+  console.log(users);
   return (
-   
-      <User 
-      meet={users} 
-      color={colors} 
-      />
-   
+    <div style={{ textAlign: "center" }}>
+      <h2>Props</h2>
+      <table
+        style={{ margin: "auto", padding: "50px", border: "1px solid black" }}
+      >
+        <thead style={{ border: "1px solid black" }}>
+          <tr>
+            <th style={{ border: "1px solid black" }}>ID</th>
+            <th style={{ border: "1px solid black" }}>Name</th>
+            <th style={{ border: "1px solid black" }}>Email</th>
+            <th style={{ border: "1px solid black" }}>Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr
+              key={user.id}
+              style={{ textAlign: "center", border: "1px solid black" }}
+            >
+              <td style={{ fontWeight: "bold", border: "1px solid black" }}>
+                {user.id}
+              </td>
+              <td style={{ fontWeight: "bold", border: "1px solid black" }}>
+                {user.name}
+              </td>
+              <td style={{ fontWeight: "bold", border: "1px solid black" }}>
+                {user.email}
+              </td>
+              <td
+                style={{
+                  color: colors.find((color) => color.id === user.id).color,
+                  fontWeight: "bold",
+                  border: "1px solid black",
+                }}
+              >
+                {colors.find((color) => color.id === user.id).color}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- props/src/User.jsx
+++ props/src/User.jsx
@@ -3,6 +3,7 @@ class User extends Component {
   render() {
     return (
       <div>
+         
         {this.props.meet.map((user) => (
           <h2 key={user.id}>{user.name}</h2>
         ))}
