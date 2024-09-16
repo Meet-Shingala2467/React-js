abffbeae20ce930c77aad1903cd40270a6c5e1f2
Meet-Shingala2467 <meetshingala5557@gmail.com>
2024-09-16 10:01:51 +0530

    1

--- pr 1.Explorer/src/Data.jsx
+++ pr 1.Explorer/src/Data.jsx
@@ -7,28 +7,97 @@ class Person extends Component {
   }
   render() {
     return (
-      <div style={{ textAlign: 'center', margin: 'auto', width: '80%' }}>
-        <h1 style={{ color: new Date().getMilliseconds() % 2 === 0 ? 'blue' : 'red', animation: 'blink 0s infinite' }}>User Information</h1>
-        <table style={{ margin: 'auto', border: '1px solid black', borderCollapse: 'collapse' }} cellSpacing="5" cellPadding="5">
+      <div style={{ textAlign: "center", margin: "auto", width: "80%" }}>
+        <h1
+          style={{
+            color: new Date().getMilliseconds() % 2 === 0 ? "blue" : "red",
+            animation: "blink 0s infinite",
+          }}
+        >
+          User Information
+        </h1>
+        <table
+          style={{
+            margin: "auto",
+            border: "1px solid black",
+            borderCollapse: "collapse",
+          }}
+          cellSpacing="5"
+          cellPadding="5"
+        >
           <thead>
             <tr>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>Grid</th>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>Name</th>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>Email</th>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>Password</th>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>Courses</th>
-              <th style={{ border: '1px solid black', backgroundColor: 'lightgray' }}>City</th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                Grid
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                Name
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                Email
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                Password
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                Courses
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  backgroundColor: "lightgray",
+                }}
+              >
+                City
+              </th>
             </tr>
           </thead>
           <tbody>
-            {this.props.Meet.map((person,i) => (
+            {this.props.Meet.map((person, i) => (
               <tr key={i}>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.grid}</td>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.name}</td>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.email}</td>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.password}</td>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.course.join(', ')}</td>
-                <td style={{ border: '1px solid black', fontWeight: 'bold' }}>{person.city}</td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.grid}
+                </td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.name}
+                </td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.email}
+                </td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.password}
+                </td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.course.join(", ")}
+                </td>
+                <td style={{ border: "1px solid black", fontWeight: "bold" }}>
+                  {person.city}
+                </td>
               </tr>
             ))}
           </tbody>
