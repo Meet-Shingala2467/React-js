8e198b3a5065c1109bafc4a36ee07fb0b81ac1eb
Meet-Shingala2467 <meetshingala5557@gmail.com>
2024-09-18 11:19:04 +0530

    18 / 9

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Useeffect from './useEffect'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Useeffect/>
+    </>
+  )
+}
+
+export default App
